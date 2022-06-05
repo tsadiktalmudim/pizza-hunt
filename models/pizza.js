f2bfc6364492c093424403f4110b7e129c95e93e
Xan <tsadiktalmudim@gmail.com>
2022-06-05 13:19:08 -0500

    added validation

--- models/pizza.js
+++ models/pizza.js
@@ -5,9 +5,13 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: "A pizza name is required",
+      trim: true,
     },
     createdBy: {
       type: String,
+      require: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     toppings: [],
